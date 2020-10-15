1007097be23fa263c46e41a60fa09a212331ab14
2310216264 <2310216264@qq.com>
2020-10-15 23:01:05 +0800

    10-15学习内容

---
+++ 10-15/demo.js
@@ -0,0 +1,1 @@
+dowond("iafasf");
---
+++ 10-15/getnew.php
@@ -0,0 +1,7 @@
+<?php
+$news = array(
+    array('title'=>'德国女总理默克尔滑雪时摔倒 盆骨断裂','date'=>'2014-1-6'),
+    array('title'=>'中日驻英外交官撰文互称对方国家为"伏地魔"','date'=>'2014-1-6'),
+);
+
+echo json_encode($news);
